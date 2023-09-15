d34374e7f65a72db8cdc532ec1eecbba2829ccb3
easterlil <129802659+easterlil@users.noreply.github.com>
2023-09-15 00:19:24 -0500

    Update infer-web.py

--- infer-web.py
+++ infer-web.py
@@ -176,8 +176,10 @@ def load_hubert():
 
 datasets_root = "datasets"
 weight_root = "/tmp/rvc/weights/"
+weight_rootinfer = "weights"
 weight_uvr5_root = "uvr5_weights"
 index_root = "/tmp/rvc/logs/"
+index_rootinfer = "./logs/"
 fshift_root = "formantshiftcfg"
 audio_root = "audios"
 audio_others_root = "audio-others"
@@ -200,14 +202,14 @@ sup_audioext = {
 
 names = [
     os.path.join(root, file)
-    for root, _, files in os.walk(weight_root)
+    for root, _, files in os.walk(weight_rootinfer)
     for file in files
     if file.endswith((".pth", ".onnx"))
 ]
 
 indexes_list = [
     os.path.join(root, name)
-    for root, _, files in os.walk(index_root, topdown=False)
+    for root, _, files in os.walk(index_rootinfer, topdown=False)
     for name in files
     if name.endswith(".index") and "trained" not in name
 ]
@@ -291,7 +293,7 @@ def update_dataset_list(name):
 def get_indexes():
     indexes_list = [
         os.path.join(dirpath, filename)
-        for dirpath, _, filenames in os.walk(index_root)
+        for dirpath, _, filenames in os.walk(index_rootinfer)
         for filename in filenames
         if filename.endswith(".index") and "trained" not in filename
     ]
@@ -976,13 +978,13 @@ def get_vc(sid, to_return_protect0, to_return_protect1):
 def change_choices():
     names = [
         os.path.join(root, file)
-        for root, _, files in os.walk(weight_root)
+        for root, _, files in os.walk(weight_rootinfer)
         for file in files
         if file.endswith((".pth", ".onnx"))
     ]
     indexes_list = [
         os.path.join(root, name)
-        for root, _, files in os.walk(index_root, topdown=False)
+        for root, _, files in os.walk(index_rootinfer, topdown=False)
         for name in files
         if name.endswith(".index") and "trained" not in name
     ]
@@ -1001,13 +1003,13 @@ def change_choices():
 def change_choices2():
     names = [
         os.path.join(root, file)
-        for root, _, files in os.walk(weight_root)
+        for root, _, files in os.walk(weight_rootinfer)
         for file in files
         if file.endswith((".pth", ".onnx"))
     ]
     indexes_list = [
         os.path.join(root, name)
-        for root, _, files in os.walk(index_root, topdown=False)
+        for root, _, files in os.walk(index_rootinfer, topdown=False)
         for name in files
         if name.endswith(".index") and "trained" not in name
     ]
@@ -1875,9 +1877,9 @@ def match_index(sid0: str) -> tuple:
     else:
         base_model_name = sid0name
 
-    sid_directory = os.path.join(index_root, base_model_name)
+    sid_directory = os.path.join(index_rootinfer, base_model_name)
     directories_to_search = [sid_directory] if os.path.exists(sid_directory) else []
-    directories_to_search.append(index_root)
+    directories_to_search.append(index_rootinfer)
 
     matching_index_files = []
 
@@ -1931,7 +1933,7 @@ def stoptraining(mim):
     )
 
 
-weights_dir = "/tmp/rvc/weights/"
+weights_dir = "weights/"
 
 
 def note_to_hz(note_name):
